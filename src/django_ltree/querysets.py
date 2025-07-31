6ead59dab9ee84bf82529e4e6140794997f180ce
hongxunming <hongxunming@gmail.com>
2025-07-31 20:37:44 +0800

    order_by_path_numbers

--- src/django_ltree/querysets.py
+++ src/django_ltree/querysets.py
@@ -12,3 +12,16 @@ class TreeQuerySet(models.QuerySet):
 
     def children(self, path: str) -> models.QuerySet["TreeModel"]:
         return self.filter(path__descendants=path, path__depth=len(path) + 1)
+    
+    def order_by_path_numbers(self):
+        """
+        36982E79-1901-4E3C-9947-3F8D0120C6BB
+        按照路径中的数字进行排序
+        将路径转换为整数数组进行排序
+        """
+        return self.extra(
+            select={'path_array': "cast(string_to_array(ltree2text(path), '.') AS integer[])",
+                    'path_level': "nlevel(path)"},
+            order_by=['path_array', 'path_level']
+        )
+
